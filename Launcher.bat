775bdbca681cff034eb24ffe800afd32a00e96ba
charmakr <lauri.sivula@gmail.com>
2018-05-21 10:50:08 +0300

    Added Updater/Launcher

---
+++ Launcher.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001
+cls
+color 0A
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo XXXXX         Luokan asteukset - Launcher 1.0            XXXXX
+echo XXXXX                      Lauri                         XXXXX
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo.
+wget "https://raw.githubusercontent.com/charmakr/Luokan-asetukset/master/Monitoriasetukset.bat" -O Monitoriasetukset.bat
+pause
+Monitoriasetukset.bat
